a5c8ccc5399478d48fa1c9e4ef10743ebe6af741
tang_huibo <1215893264@qq.com>
2019-06-22 02:19:31 +0800

    上传自动提交脚本

--- commit.sh
+++ commit.sh
@@ -1,11 +1,21 @@
+#创建文件
 gitbook build
+#添加所有文件到暂存区
 git add .
+#提交注释
 git commit -m "$1"
+#推到远端
 git push
-cd _book
-git init
-git remote add origin git@github.com:tanghuibo/notes.git
-git checkout -b gh-pages
+#进入发布目录
+cd publish
+#删除所有非隐藏文件
+sudo rm -rf *
+#拷贝_book下的文件
+cp -r ../_book/. ./
+#添加所有文件到暂存区
 git add .
+#提交注释
 git commit -m "$1"
-git push --set-upstream origin gh-pages
+#推到远端
+git push
+
