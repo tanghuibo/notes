109f1ec64c20aacc9f0eed5b51481c7252f45cc3
tang_huibo <1215893264@qq.com>
2019-06-22 01:40:43 +0800

    ./commit.sh

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
 gitbook build
-git add *
+git add .
 git commit -m "$0"
 git push --set-upstream origin master
 cd _book
-git add *
+git add .
 git commit -m "$0"
 git push --set-upstream origin gh-pages
