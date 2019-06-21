a72e7873fa55718a059eac1ce3f401bb120053d6
tang_huibo <1215893264@qq.com>
2019-06-22 01:50:53 +0800

    上传自动提交脚本

--- commit.sh
+++ commit.sh
@@ -1,11 +1,11 @@
 gitbook build
 git add .
 git commit -m "$1"
-git push --set-upstream origin master
+git push
 cd _book
 git init
 git remote add origin git@github.com:tanghuibo/notes.git
 git checkout -b gh-pages
 git add .
 git commit -m "$1"
-git push
+git push --set-upstream origin gh-pages
