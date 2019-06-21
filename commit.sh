8b1c9ebb2fd72a2da9b978e8380326539409085e
tang_huibo <1215893264@qq.com>
2019-06-22 02:46:35 +0800

    上传自动提交脚本

--- commit.sh
+++ commit.sh
@@ -1,16 +1,3 @@
-#创建文件
-gitbook build
-#添加所有文件到暂存区
-git add .
-#提交注释
-git commit -m "$1"
+#!/bin/bash
 #推到远端
-git push
-#进入发布目录
-cd publish
-#删除所有非隐藏文件
-sudo rm -rf *
-#拷贝_book下的文件
-cp -r ../_book/. ./
-#推到远端
-source publish/push "$1"
+publish/.push.sh "$1"
