6485db2bb5b7fe86387cfe11bee4653b8f9b0e81
tang_huibo <1215893264@qq.com>
2019-06-22 01:49:07 +0800

    上传自动提交脚本

--- commit.sh
+++ commit.sh
@@ -1,8 +1,11 @@
 gitbook build
 git add .
-git commit -m "$0"
+git commit -m "$1"
 git push
 cd _book
+git init
+git remote add origin git@github.com:tanghuibo/notes.git
+git checkout -b gh-pages
 git add .
-git commit -m "$0"
+git commit -m "$1"
 git push
