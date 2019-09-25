19f8b62bc7f99484d612b14561f4d9b2fd1d7d96
tang_huibo <1215893264@qq.com>
2019-09-26 01:23:49 +0800

    修改commit.sh

--- commit.sh
+++ commit.sh
@@ -8,11 +8,11 @@ git commit -m "$1"
 #推到远端
 git push
 #进入发布目录
-cd publish
+cd ../nodes_publish/
 #删除所有非隐藏文件
 sudo rm -rf *
 #拷贝_book下的文件
-cp -r ../_book/. ./
+cp -r ../notes/_book/. ./
 #提示用户提交文件
 echo "请手动运行: ./.push.sh \"$1\" (命令已被复制到剪切板)" 
 echo "./.push.sh \"$1\"" | xclip -selection clipboard 
